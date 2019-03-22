43f84a629b247b2e4f713b1fd893fff00b823725
Thang <Jubal@jubals-mac.local>
2019-03-22 10:23:25 -0700

    Initial Commit

---
+++ TableColumn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableColumn
+//
+//  Created by JubalThang on 3/22/19.
+//  Copyright © 2019 Jubal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
